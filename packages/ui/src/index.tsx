3aed0c210b2bc99b106f5ddc63d28dfd2c3b9de5
ercusz <57826174+ercusz@users.noreply.github.com>
2023-05-05 10:37:49 +0700

    feat(UI): add UI components from shadcn/ui

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -4,7 +4,25 @@ import "./styles.css";
 // libs
 export * from "@/lib/utils";
 
+// hooks
+export * from "@/components/ui/use-toast";
+
 // components
+export * from "@/components/ui/accordion";
+export * from "@/components/ui/avatar";
+export * from "@/components/ui/badge";
 export * from "@/components/ui/button";
 export * from "@/components/ui/card";
+export * from "@/components/ui/checkbox";
+export * from "@/components/ui/input";
+export * from "@/components/ui/label";
 export * from "@/components/ui/scroll-area";
+export * from "@/components/ui/select";
+export * from "@/components/ui/separator";
+export * from "@/components/ui/skeleton";
+export * from "@/components/ui/tabs";
+export * from "@/components/ui/toast";
+export * from "@/components/ui/toaster";
+export * from "@/components/ui/command";
+export * from "@/components/ui/popover";
+export * from "@/components/ui/dialog";
