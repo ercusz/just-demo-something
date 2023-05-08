f89f66888a79db4fc750b650c320ce4d6075e8c3
ercusz <57826174+ercusz@users.noreply.github.com>
2023-05-08 14:23:49 +0700

    feat: add PrimaryLayout

---
+++ apps/nextjs/src/components/cards/UserCard.tsx
@@ -0,0 +1,70 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@acme/ui";
+import { UserButton, useAuth } from "@clerk/nextjs";
+import { Link } from "lucide-react";
+import { api } from "~/utils/api";
+
+const UserCard: React.FC = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+  const { data: employeeData } = api.employee.byId.useQuery(undefined, {
+    enabled: !!isSignedIn,
+  });
+
+  return (
+    <>
+      {isSignedIn && isLoaded && (
+        <Card>
+          <CardHeader>
+            <CardTitle>สวัสดี 👋🏻</CardTitle>
+            <CardDescription>
+              ตำแหน่งของคุณ คือ {employeeData?.position.name}
+              {employeeData?.position?.name === "Manager" && (
+                <>
+                  <br />
+                  <span>แผนก {employeeData.department.name}</span>
+                </>
+              )}
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-6">
+            <div className="flex items-center justify-between space-x-4">
+              <div className="flex items-center space-x-4">
+                <UserButton
+                  appearance={{
+                    elements: {
+                      userButtonAvatarBox: {
+                        width: "3rem",
+                        height: "3rem",
+                      },
+                    },
+                  }}
+                />
+                <div>
+                  <p className="text-sm font-medium leading-none">
+                    {`${employeeData?.firstName} ${employeeData?.lastName}`}
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    {employeeData?.email}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      )}
+      {!isSignedIn && isLoaded && (
+        <Button asChild>
+          <Link href="/sign-in">Sign In</Link>
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default UserCard;
--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,19 +1,32 @@
 // src/pages/_app.tsx
 import { Toaster } from "@acme/ui";
 import { ClerkProvider } from "@clerk/nextjs";
-import type { AppType } from "next/app";
 import { api } from "~/utils/api";
 import "../styles/globals.css";
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
 // include styles from the ui package
 import "@acme/ui/styles.css";
 
-const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
       <Toaster />
     </ClerkProvider>
   );
-};
+}
 
 export default api.withTRPC(MyApp);
