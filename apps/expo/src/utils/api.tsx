31788030a166a46d17fec1b19b73197a2831fe00
ercusz <57826174+ercusz@users.noreply.github.com>
2023-05-15 15:37:14 +0700

    fix: getAuth not works when create trpc context
    trpc api client must have authToken in header

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -1,56 +1,53 @@
-import { type AppRouter } from "@acme/api";
+import type { AppRouter } from "@acme/api";
+import { createTRPCReact } from "@trpc/react-query";
+/**
+ * Extend this function when going to production by
+ * setting the baseUrl to your production API URL.
+ */
+import Constants from "expo-constants";
+/**
+ * A wrapper for your app that provides the TRPC context.
+ * Use only in _app.tsx
+ */
+import { transformer } from "@acme/api/transformer";
+import { useAuth } from "@clerk/clerk-expo";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
-import { createTRPCReact } from "@trpc/react-query";
-import Constants from "expo-constants";
 import React from "react";
-import superjson from "superjson";
 
 /**
  * A set of typesafe hooks for consuming your API.
  */
 export const api = createTRPCReact<AppRouter>();
-export { type RouterInputs, type RouterOutputs } from "@acme/api";
 
-/**
- * Extend this function when going to production by
- * setting the baseUrl to your production API URL.
- */
 const getBaseUrl = () => {
   /**
    * Gets the IP address of your host-machine. If it cannot automatically find it,
    * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
    * you don't have anything else running on it, or you'd have to change it.
-   *
-   * **NOTE**: This is only for development. In production, you'll want to set the
-   * baseUrl to your production API URL.
    */
-  const debuggerHost =
-    Constants.manifest?.debuggerHost ??
-    Constants.manifest2?.extra?.expoGo?.debuggerHost;
-  const localhost = debuggerHost?.split(":")[0];
-  if (!localhost) {
-    // return "https://your-production-url.com";
-    throw new Error(
-      "Failed to get localhost. Please point to your production server.",
-    );
-  }
+  const localhost = Constants.manifest?.debuggerHost?.split(":")[0];
+  if (!localhost)
+    throw new Error("failed to get localhost, configure it manually");
   return `http://${localhost}:3000`;
 };
 
-/**
- * A wrapper for your app that provides the TRPC context.
- * Use only in _app.tsx
- */
-export const TRPCProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+export const TRPCProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const { getToken } = useAuth();
   const [queryClient] = React.useState(() => new QueryClient());
   const [trpcClient] = React.useState(() =>
     api.createClient({
-      transformer: superjson,
+      transformer,
       links: [
         httpBatchLink({
+          async headers() {
+            const authToken = await getToken();
+            return {
+              Authorization: authToken ?? undefined,
+            };
+          },
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
