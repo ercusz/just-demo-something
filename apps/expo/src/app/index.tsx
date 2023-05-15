26e4a16a39dc6b2de7088fc2408804eff4dfc169
ercusz <57826174+ercusz@users.noreply.github.com>
2023-05-15 15:39:33 +0700

    feat: display employee data in expo

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,99 +1,38 @@
 import { useAuth, useUser } from "@clerk/clerk-expo";
-import { FlashList } from "@shopify/flash-list";
-import { Stack, useRouter } from "expo-router";
+import { Stack } from "expo-router";
 import React from "react";
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { api, type RouterOutputs } from "~/utils/api";
+import { api } from "~/utils/api";
 
-const PostCard: React.FC<{
-  post: RouterOutputs["post"]["all"][number];
-  onDelete: () => void;
-}> = ({ post, onDelete }) => {
-  const router = useRouter();
+const EmployeeCard: React.FC = () => {
+  const { userId, isSignedIn } = useAuth();
+
+  const { data } = api.employee.byId.useQuery(
+    {
+      userId: userId ?? "",
+    },
+    {
+      enabled: !!isSignedIn,
+    },
+  );
 
   return (
     <View className="flex flex-row rounded-lg bg-white/10 p-4">
       <View className="flex-grow">
-        <TouchableOpacity onPress={() => router.push(`/post/${post.id}`)}>
-          <Text className="text-xl font-semibold text-pink-400">
-            {post.title}
-          </Text>
-          <Text className="mt-2 text-white">{post.content}</Text>
-        </TouchableOpacity>
+        <Text className="text-xl font-semibold text-pink-400">
+          Your position is {data?.position.name}
+        </Text>
+        <Text className="text-lg mt-2 text-white">
+          Department: {data?.department.name}
+        </Text>
       </View>
-      <TouchableOpacity onPress={onDelete}>
-        <Text className="font-bold uppercase text-pink-400">Delete</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-const CreatePost: React.FC = () => {
-  const utils = api.useContext();
-
-  const [title, setTitle] = React.useState("");
-  const [content, setContent] = React.useState("");
-
-  const { mutate, error } = api.post.create.useMutation({
-    async onSuccess() {
-      setTitle("");
-      setContent("");
-      await utils.post.all.invalidate();
-    },
-  });
-
-  return (
-    <View className="mt-4">
-      <TextInput
-        className="mb-2 rounded bg-white/10 p-2 text-white"
-        placeholderTextColor="rgba(255, 255, 255, 0.5)"
-        value={title}
-        onChangeText={setTitle}
-        placeholder="Title"
-      />
-      {error?.data?.zodError?.fieldErrors.title && (
-        <Text className="mb-2 text-red-500">
-          {error.data.zodError.fieldErrors.title}
-        </Text>
-      )}
-      <TextInput
-        className="mb-2 rounded bg-white/10 p-2 text-white"
-        placeholderTextColor="rgba(255, 255, 255, 0.5)"
-        value={content}
-        onChangeText={setContent}
-        placeholder="Content"
-      />
-      {error?.data?.zodError?.fieldErrors.content && (
-        <Text className="mb-2 text-red-500">
-          {error.data.zodError.fieldErrors.content}
-        </Text>
-      )}
-      <TouchableOpacity
-        className="rounded bg-pink-400 p-2"
-        onPress={() => {
-          mutate({
-            title,
-            content,
-          });
-        }}
-      >
-        <Text className="font-semibold text-white">Publish post</Text>
-      </TouchableOpacity>
     </View>
   );
 };
 
 const Index = () => {
-  const utils = api.useContext();
-
-  const postQuery = api.post.all.useQuery();
-
-  const deletePostMutation = api.post.delete.useMutation({
-    onSettled: () => utils.post.all.invalidate(),
-  });
-
-  const { user } = useUser();
+  const { user, isLoaded } = useUser();
   const { signOut } = useAuth();
 
   const onSignOutPress = async () => {
@@ -141,28 +80,7 @@ const Index = () => {
           )}
         </View>
 
-        <View className="py-2 flex flex-row justify-between items-center">
-          <Text className="text-3xl font-semibold text-white">Posts</Text>
-          <TouchableOpacity onPress={() => void utils.post.all.invalidate()}>
-            <Text className="font-bold uppercase text-pink-400 text-right underline">
-              Refresh Posts
-            </Text>
-          </TouchableOpacity>
-        </View>
-
-        <FlashList
-          data={postQuery.data}
-          estimatedItemSize={20}
-          ItemSeparatorComponent={() => <View className="h-2" />}
-          renderItem={(p) => (
-            <PostCard
-              post={p.item}
-              onDelete={() => deletePostMutation.mutate(p.item.id)}
-            />
-          )}
-        />
-
-        <CreatePost />
+        {isLoaded && <EmployeeCard />}
       </View>
     </SafeAreaView>
   );
